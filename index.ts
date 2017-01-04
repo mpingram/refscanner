ad30a58f0acac0bf435f62846b79918a422ae763
Michael Ingram <mpingram92@gmail.com>
2017-01-04 16:51:30 -0600

    Begin work on refchecking class

---
+++ index.ts
@@ -0,0 +1,14 @@
+// rough outline of class:
+// - initialized with in-text reference style, biblio style, and file paths to main text and bibliography.
+// - reads in biblio and finds set of biblio references according to reference style
+// - checks main text for biblio references
+// - checks main text for inline references - do they exist in biblio references? -- going backwards is the hard part. We'll need titles for the MLA and Chicago citations. -- re titles: should store as split array by words, look for first 3 elements? What about one-word titles? Ughhhhhhhhhhhghghghg
+'use strict';
+
+module.exports = RefChecker;
+
+class RefChecker {
+
+}
+
+
